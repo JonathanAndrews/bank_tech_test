da8a5e0ec1220e1827f0ac969b16abec30eddf3f
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-11-05 16:08:58 +0000

    Extracted Statement from the Account Class
    - Statement instance injected into Account class
    - Tests check that Statement#print_out is called with the transactions array.

--- lib/account.rb
+++ lib/account.rb
@@ -2,23 +2,16 @@ require 'date'
 
 class Account
 
-  attr_reader :balance, :transactions
+  attr_reader :balance, :transactions, :statement
 
-  def initialize
+  def initialize(statement = Statement.new)
     @transactions = []
     @balance = 0 
+    @statement = statement
   end
 
   def bank_statement
-    string_of_table = "date || credit || debit || balance"
-    transactions.each do |transaction|
-      date = transaction[:date]
-      credit = transaction[:credit]
-      debit = transaction[:debit]
-      balance = transaction[:balance]
-      string_of_table += "\n#{date} || #{credit} || #{debit} || #{balance}"
-    end
-    string_of_table
+    statement.print_out(transactions)
   end
 
   def deposit(money)
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,18 +1,25 @@
-require "account"
+require 'account'
 
 describe Account do
   context 'Brand new bank account' do
-    let(:account) { described_class.new }
+
+    let(:statement_double) { double :Statement }
+    let(:account) { described_class.new(statement_double) }
+    let(:empty_array) { [] }
+
     describe 'Account#bank_statement' do
-      it "prints out blank bank statement" do
-        statement = account.bank_statement
-        expect(statement).to eq("date || credit || debit || balance")
+      it "calls Statement#print_out" do
+        expect(statement_double).to receive(:print_out).with(empty_array)
+        
+        account.bank_statement
       end
     end 
   end
 
   context 'Transactions' do
+
     let(:account) { described_class.new }
+
     describe 'Account#deposit' do
       it 'makes a credit transaction object' do
         allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
@@ -22,15 +29,6 @@ describe Account do
       end
     end 
 
-    describe 'Account#bank_statement' do
-      it 'prints out bank statement with one row after deposit' do
-        allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
-        string_of_table = "date || credit || debit || balance\n05/11/2018 || 1 ||  || 1"
-        account.deposit(1)
-        expect(account.bank_statement).to eq(string_of_table)
-      end
-    end
-
     describe 'Account#withdraw' do
       it 'makes a debit transaction object' do
         allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
@@ -39,15 +37,5 @@ describe Account do
         expect(account.withdraw(1)).to eq([returned_hash])
       end
     end 
-
-    describe 'Account#withdraw' do
-      it 'prints out bank statement with one row after withdraw' do
-        allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
-        string_of_table = "date || credit || debit || balance\n05/11/2018 ||  || 1 || -1"
-        account.withdraw(1)
-        expect(account.bank_statement).to eq(string_of_table)
-      end
-    end 
-
   end
 end
