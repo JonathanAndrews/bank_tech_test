2ea2a2422a6d48ecac4e17d505a5bda6adf6c80d
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-11-05 13:20:17 +0000

    stubbed Date in tests

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -13,14 +13,18 @@ describe ATM do
   context 'Transactions' do
     let(:atm) { described_class.new }
     describe 'ATM#deposit' do
-      it "makes a credit transaction object" do
+      it 'makes a credit transaction object' do
+        allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
+
         returned_hash = { date: '05/11/2018', credit: 1, balance: 1 }
         expect(atm.deposit(1)).to eq([returned_hash])
       end
     end 
 
     describe 'ATM#bank_statement' do
-      it "prints out bank statement with one row after deposit" do
+      it 'prints out bank statement with one row after deposit' do
+        allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
+
         atm.deposit(1)
         expect(atm.bank_statement).to eq("date || credit || debit || balance\n05/11/2018 || 1 ||  || 1")
       end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -15,6 +15,8 @@ context "Feature Tests" do
       let(:atm) { described_class.new }
       describe 'ATM#deposit' do
         it 'makes a credit transaction object' do
+          allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
+
           returned_hash = { date: '05/11/2018', credit: 1, balance: 1 }
           expect(atm.deposit(1)).to eq([returned_hash])
         end
@@ -22,6 +24,8 @@ context "Feature Tests" do
   
       describe 'ATM#bank_statement' do
         it 'prints out bank statement with one row after deposit' do
+          allow(Date).to receive(:today).and_return(Date.new(2018, 11, 5))
+
           atm.deposit(1)
           expect(atm.bank_statement).to eq("date || credit || debit || balance\n05/11/2018 || 1 ||  || 1")
         end
